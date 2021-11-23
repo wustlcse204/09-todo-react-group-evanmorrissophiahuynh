11aa5737781c404a0417cfc57816f66c6e19d6d3
EvanMorris0622 <evan.morris@wustl.edu>
2021-11-23 15:54:28 -0600

    update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>ToDo App</h1>
+        <h1>Evan Morris, Sophia Huynh TODO App</h1>
       </div>
     );
   }
