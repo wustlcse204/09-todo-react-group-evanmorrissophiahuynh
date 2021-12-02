48ab432c20c27230d2483ea184b1a216ed54e7bf
EvanMorris0622 <evan.morris@wustl.edu>
2021-12-02 14:36:35 -0600

    Updates

--- precache-manifest.b296c1e7432888f6292bbbba5c8a9be9.js
+++ precache-manifest.b296c1e7432888f6292bbbba5c8a9be9.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "4fa1d4c6da9845c10b3f",
+    "revision": "fcebbb16bc4a1791e0a3",
     "url": "/09-todo-react-group-evanmorrissophiahuynh/static/css/main.fe72fba8.chunk.css"
   },
   {
-    "revision": "4fa1d4c6da9845c10b3f",
-    "url": "/09-todo-react-group-evanmorrissophiahuynh/static/js/main.4fa1d4c6.chunk.js"
+    "revision": "fcebbb16bc4a1791e0a3",
+    "url": "/09-todo-react-group-evanmorrissophiahuynh/static/js/main.fcebbb16.chunk.js"
   },
   {
     "revision": "939cbde5f9c76620fa49",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/09-todo-react-group-evanmorrissophiahuynh/static/js/runtime~main.c9a922b3.js"
   },
   {
-    "revision": "3c1a94bd8fd81986d9173cca843e5512",
+    "revision": "02b26e943f1f4dbd4f634c6853c0a059",
     "url": "/09-todo-react-group-evanmorrissophiahuynh/index.html"
   }
 ];
