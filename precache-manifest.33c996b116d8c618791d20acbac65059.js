a0e30f0231aa995f6885b3be7f1896958639c347
EvanMorris0622 <evan.morris@wustl.edu>
2021-12-02 15:07:53 -0600

    Updates

--- precache-manifest.33c996b116d8c618791d20acbac65059.js
+++ precache-manifest.33c996b116d8c618791d20acbac65059.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "edb5530fc996938c6e91",
+    "revision": "b1ad94d4867cedd9e8e9",
     "url": "/09-todo-react-group-evanmorrissophiahuynh/static/css/main.fe72fba8.chunk.css"
   },
   {
-    "revision": "edb5530fc996938c6e91",
-    "url": "/09-todo-react-group-evanmorrissophiahuynh/static/js/main.edb5530f.chunk.js"
+    "revision": "b1ad94d4867cedd9e8e9",
+    "url": "/09-todo-react-group-evanmorrissophiahuynh/static/js/main.b1ad94d4.chunk.js"
   },
   {
     "revision": "939cbde5f9c76620fa49",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/09-todo-react-group-evanmorrissophiahuynh/static/js/runtime~main.c9a922b3.js"
   },
   {
-    "revision": "7b5c4a56649047738793b9f0b8999426",
+    "revision": "b7db9e854f1e42d72e83a4bc6eb6ed2e",
     "url": "/09-todo-react-group-evanmorrissophiahuynh/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/09-todo-react-group-evanmorrissophiahuynh/precache-manifest.25b1c29256c4ac5a0f01b9253e5ca3c8.js"
+  "/09-todo-react-group-evanmorrissophiahuynh/precache-manifest.33c996b116d8c618791d20acbac65059.js"
 );
 
 workbox.clientsClaim();
